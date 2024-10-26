499bfd105f8b1ff1e6369808a74b7b1ba7829fd7
Jocelyne Murphy <jocelynemurphy@gmail.com>
2024-10-26 14:42:29 -0400

    Initial Commit

---
+++ HaptiSteer Controller/HaptiSteer_ControllerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HaptiSteer_ControllerApp.swift
+//  HaptiSteer Controller
+//
+//  Created by Coding on 2024-10-26.
+//
+
+import SwiftUI
+
+@main
+struct HaptiSteer_ControllerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
