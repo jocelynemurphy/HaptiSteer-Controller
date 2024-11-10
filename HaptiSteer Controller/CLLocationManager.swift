90cf2fa82a3557cc7e2ea273ca2d79b430090155
Jocelyne Murphy <jocelynemurphy@gmail.com>
2024-11-09 21:11:52 -0500

    distance calculation wip

---
+++ HaptiSteer Controller/CLLocationManager.swift
@@ -0,0 +1,43 @@
+import SwiftUI
+import CoreLocation
+
+class LocationTrackerViewController: NSObject, ObservableObject, CLLocationManagerDelegate {
+    
+    private let locationManager = CLLocationManager()
+    
+    // Observable properties that SwiftUI views can observe
+    @Published var currentLocation: CLLocation? = nil
+    
+    override init() {
+        super.init()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+        locationManager.requestWhenInUseAuthorization()  // For foreground usage
+        locationManager.requestAlwaysAuthorization()    // For background usage
+        locationManager.startUpdatingLocation()
+    }
+    
+    // CLLocationManagerDelegate method to handle location updates
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else { return }
+        currentLocation = location  // Update the location property
+    }
+    
+    // CLLocationManagerDelegate method to handle errors
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Error occurred: \(error.localizedDescription)")
+    }
+    
+    // Handle authorization status changes
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        switch status {
+        case .authorizedWhenInUse, .authorizedAlways:
+            print("Location permission granted.")
+            locationManager.startUpdatingLocation()  // Start updating location if permission is granted
+        case .denied, .restricted:
+            print("Location permission denied or restricted.")
+        default:
+            print("Location permission status unknown.")
+        }
+    }
+}
--- HaptiSteer Controller/ContentView.swift
+++ HaptiSteer Controller/ContentView.swift
@@ -1,23 +1,6 @@
 import SwiftUI
 import CoreBluetooth
-
-//struct Wrapper: Codable {
-//    let items: [Question]
-//}
-
-//struct Question: Codable {
-//    let score: Int
-//    let title: String
-//}
-
-//func performAPICall() async throws -> Question {
-//    let url = URL(string: "https://maps.googleapis.com/maps/api/directions/json")!
-//    let (data, _) = try await URLSession.shared.data(from: url)
-//    let wrapper = try JSONDecoder().decode(Wrapper.self, from: data)
-//    return wrapper.items[0]
-//}
-
-
+import CoreLocation
 // ________________________________________________________________________
 // SET UP ALL STRUCTS
 // Root structure
@@ -138,8 +121,111 @@ func performAPICall(origin: String, destination: String, mode: String, apiKey: S
     return response
 }
 
+let maneuverMapping: [String: Int] = [ // mapping of all the different maneuvers to the number of pulses and instruction for pulsing
+    // L = -1, M = 0, R = 1. We may send over bluetooth the strings but this will roughly map which pulses.
+    "turn-slight-left": 1, // 2 L
+    "turn-sharp-left": 2, // 5 L
+    "uturn-left": 3, // 3L, 3L, 3L, (repeat until turned around) -> how does it know you've turned around?
+    "turn-left": 4, // 5 L
+    
+    "turn-slight-right": 5, // 2 R
+    "turn-sharp-right": 6, // 5 R
+    "uturn-right": 7, // 3R, 3R, 3R
+    "turn-right": 8, // 5R
+    
+    "straight": 9, // 2 S
+    "ramp-left": 10, // 2 R
+    "ramp-right": 11, // 2 R
+    "merge": 12, // 2 R or L (is there no merge command for R or L?)
+    
+    "fork-left": 13, // 2 L
+    "fork-right": 14, // 2 R
+    "ferry": 15, // N/A
+    "ferry-train": 16, // N/A
+    
+    "roundabout-left": 17, // 3L, 3L, 3L, -> could we know when they should signal right and exit?
+    "roundabout-right": 18, // 5 R
+    "end-of-road-left": 19, // 2L
+    "end-of-road-right": 20, // 2R
+    
+    "take-exit-left": 21, // 5L
+    "take-exit-right": 22, // 5R
+    "take-exit": 23, // 5R --> discuss this onw n
+    "take-fork-left": 24, // 2L
+    "take-fork-right": 25, // 2 R
+    
+    "head": 26, // ?
+    "keep-left": 27, // 1 L
+    "keep-right": 28, // 1 R
+    "continue": 29 // 1 BOTH
+]
+
+func calculateDistance(directions: DirectionsResponse){
+    @StateObject var locationManager = LocationTrackerViewController()
+    
+//    for step in steps{
+//        // do polling every 1 second
+//        Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { timer in
+//            print("hello")
+//        }
+//        // find current location
+//        if let location = locationManager.currentLocation {
+//            var curr_lat = location.coordinate.latitude
+//            var curr_lng = location.coordinate.longitude
+//        } else {
+//            print("Fetching location...")
+//        }
+//        
+//        // do distance calculation
+//        var distance_remaining = 5 - 3
+//        
+//    }
+}
+    
+
+func sendVibrations(result: DirectionsResponse) {
+    let steps = result.routes[0].legs[0].steps
+        
+    for step in steps{
+        var distance_remaining = step.distance.value
+        let m_per_iteration = 5
+        var instruction_given = 0
+                
+        while distance_remaining > 0 {
+            print(distance_remaining, "m remaining.")
+            if distance_remaining < 10{
+                print("🚨🚨🚨🚨🚨")
+            }
+            else if distance_remaining < 25{
+                print("📳📳📳")
+            }
+            else if distance_remaining < 100{
+                print("...")
+                if instruction_given == 0{
+                    print("In ", distance_remaining, "m, ", step.maneuver)
+                }
+                instruction_given += 1
+            }
+            // else, no vibration, subtract distance
+            distance_remaining = distance_remaining - m_per_iteration
+        }
+        
+        print(step.maneuver)
+        // no distance remaining, go to the next step.
+    }
+}
+
+func sendManeuver(maneuver: String?){
+    print(maneuver)
+}
+
 struct ContentView: View {
     @StateObject private var bleManager = BLEManager()
+    @StateObject var locationManager = LocationTrackerViewController()
+    @State var message: String = "Waiting for message..."
+    @State var timer: Timer? = nil
+    @State var isTimerRunning = 1  // Helper variable to track timer state
+
     
     // calling the api
     var body: some View {
@@ -170,32 +256,94 @@ struct ContentView: View {
             // Button to call API
             Button(action: {
                 Task {
-//                    try await print(performAPICall())
                     do {
                         let result = try await performAPICall(
                             origin: "engineering+7+university+of+waterloo",
                             destination: "121+columbia+st+w+waterloo",
                             mode: "driving",
-                            apiKey: "AIzaSyDb72KOqV8C8PKltAtzuQ8toUYMzuMrBQQ"
+                            apiKey: "AIzaSyCuOkgpExbObB7dnwHbGCmT31vxEdZFJrw"
                         )
-                        
-                        let steps = result.routes[0].legs[0].steps
-                        
-                        for step in steps{
-                            print(step.distance, step.maneuver)
-                        }
+                        // take the set of instructions and output vibrations accordingly
+                        sendVibrations(result: result);
+                    
                     } catch {
                         print("Error fetching directions: \(error)")
                     }
                 }
             }) {
-                Text("Call API")
+                Text("Get Directions")
                     .padding()
                     .background(Color.blue)
                     .foregroundColor(.white)
                     .cornerRadius(10)
             }
-        }
+        
+
+            Button(action: {
+                Task {
+                    if let location = locationManager.currentLocation {
+                        message = "Current Location: \(location.coordinate.latitude), \(location.coordinate.longitude)"
+                    } else {
+                        message = "Fetching location..."
+                    }}
+                
+            }) {
+                Text("Get Location")
+                    .padding()
+                    .background(Color.red)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+            }
+            
+           
+            // BUTTON TO TOGGLE DISTANCE CALCULATION ON AND OFF
+            Button(action: {
+                Task {
+                    do {
+                        let directions = try await performAPICall(
+                            origin: "engineering+7+university+of+waterloo",
+                            destination: "121+columbia+st+w+waterloo",
+                            mode: "driving",
+                            apiKey: "AIzaSyCuOkgpExbObB7dnwHbGCmT31vxEdZFJrw"
+                        )
+                        // get the steps
+                        var steps = directions.routes[0].legs[0].steps
+
+                        for step in steps {
+                            // check to see if the timer is on or off
+                            if timer == nil {
+                                // Start the timer
+                                timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { _ in
+                                    print("Timer is running")
+                                    
+                                    calculateDistance(directions: directions)
+                                }
+                            } else {
+                                // Stop the timer if it's already running
+                                timer?.invalidate()
+                                timer = nil
+                                print("Next Step")
+                            }
+                        }
+                    } catch {
+                        print("Error fetching directions: \(error)")
+                    }
+                }
+                    
+            }) {
+                Text("Start live distance calculation")
+                    .padding()
+                    .background(Color.red)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+            }
+
+            // send messages to be visible in the app
+            TextEditor(text: $message)
+                            .frame(height: 200) // Height of the message box
+                            .border(Color.gray, width: 1) // Optional border to define the box
+                            .padding()
+            }
         .onAppear {
             bleManager.startScanning()
         }
